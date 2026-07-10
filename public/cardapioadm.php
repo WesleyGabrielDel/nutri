e67cce9e8f15a96ae846a84049f3f3ec76412ee6
Kauã Sanches <119595660+kauasanches@users.noreply.github.com>
2026-07-10 16:34:51 -0300

    Atualização do Design e correções de bugs

--- public/cardapioadm.php
+++ public/cardapioadm.php
@@ -9,16 +9,16 @@
 
 <title>NutriFlow | Administração do Cardápio</title>
 
-<link rel="stylesheet" href="static/css/cardapioadm.css">
-
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500&display=swap"
 rel="stylesheet">
 
-<link rel="stylesheet"
+    <link rel="stylesheet"
 href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
+
+    <link rel="stylesheet" href="static/css/tokens.css">
+    <link rel="stylesheet" href="static/css/cardapioadm.css">
 
 </head>
 
